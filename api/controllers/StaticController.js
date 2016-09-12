f92c0577495f1b14658192d11fd2df38659843c5
eSsXiV <essxiv@gmail.com>
2016-09-12 13:06:40 -0700

    static

---
+++ api/controllers/StaticController.js
@@ -0,0 +1,11 @@
+/**
+ * StaticController
+ *
+ * @description :: Server-side logic for managing statics
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
